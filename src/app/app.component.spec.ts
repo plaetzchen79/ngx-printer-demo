14a2bed91aefda6049d20718c5cc33d8bf68d035
plaetzchen79 <andre.wollny@aareon.com>
2020-05-30 07:23:29 +0200

    change test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -28,6 +28,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ngx-printer-demo!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ngx-printer demo-page!');
   });
 });
